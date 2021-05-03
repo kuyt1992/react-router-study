84f0cb12ea9198c5509108695925f2800db393f9
kuyt1992 <51324232+kuyt1992@users.noreply.github.com>
2021-05-03 18:32:01 +0900

    Initial commit

---
+++ src/Page1DetailB.jsx
@@ -0,0 +1,13 @@
+import { useHistory } from "react-router-dom";
+
+export const Page1DetailB = () => {
+  const history = useHistory();
+  const onClickBack = () => history.goBack();
+
+  return (
+    <div>
+      <h1>Page1DetailBページです</h1>
+      <button onClick={onClickBack}>戻る</button>
+    </div>
+  );
+};
